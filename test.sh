e73be9f56f03d7dbbe4e9580313bc684a8cfa64a
Nanasmd <81428378+Nanasmd@users.noreply.github.com>
2024-01-16 14:07:54 +0100

    Add files via upload

--- test.sh
+++ test.sh
@@ -11,12 +11,15 @@ password=$2
 bonus=$3
 
 # Launch the IRC server
+# opens a new GNOME terminal and runs the IRC server (ircserv)
+# with valgrind for memory leak checking, using the provided port and password.
 gnome-terminal -- bash -c "valgrind --track-fds=yes ./ircserv $port $password; exec bash"
 
-# Wait for the server to start
+# Wait 2 sec to ensure the server starts properly
 sleep 2
 
-# Launch two instances of irssi, each in a separate terminal window, connecting to the server
+# Launch two instances of irssi clients, 
+# each in a separate terminal window, connecting to the server
 for i in {1..2}; do
   gnome-terminal -- bash -c "irssi -c localhost -p $port -w $password; exec bash"
   sleep 1
